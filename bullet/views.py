bb7c7233d4b48f3a4f96b101655aec46537981e5
MokkoFm <mokkofmpoetry@gmail.com>
2020-11-01 00:40:48 +0200

    add partners and gallery

--- bullet/views.py
+++ bullet/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render, redirect
-from . models import Service
+from django.shortcuts import render
+from . models import Service, Partner, Gallery
 from . forms import CustomerForm
 from django.contrib import messages
-from django.http import HttpResponseRedirect
 
 
 def index(request):
     services = Service.objects.all()
+    partners = Partner.objects.all()
+    images = Gallery.objects.all()
+
     form = CustomerForm()
+    messages.success(request, '')
     if request.method == "POST":
         form = CustomerForm(request.POST)
         if form.is_valid:
             messages.success(request, 'Your message has been sent. Thank you!')
             form.save()
             form = CustomerForm()
+            messages.success(request, '')
 
     context = {
         "services": services,
+        "partners": partners,
+        "images": images,
         "form": form
     }
     return render(request, "index.html", context=context)

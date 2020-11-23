a3bfa1b9ac2eb365bad738dce8d1775666cc978b
MokkoFm <mokkofmpoetry@gmail.com>
2020-11-23 15:38:34 +0200

    add settings for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bullettime.wsgi --log-file -
--- bullettime/settings.py
+++ bullettime/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY", "mokko2070")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bullet-time.herokuapp.com/', '127.0.0.1:8000/']
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bullettime.urls'

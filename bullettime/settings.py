267917767fbd8b5a344f26d043547404718c0824
MokkoFm <mokkofmpoetry@gmail.com>
2020-11-26 00:38:29 +0200

    add django-compression-middleware

--- bullettime/settings.py
+++ bullettime/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'compression_middleware.middleware.CompressionMiddleware',
 ]
 
 ROOT_URLCONF = 'bullettime.urls'
